f9460275ca2011eaa5c036f592e7162ab0a2be68
OnionTiger <lijundong@inke.cn>
2025-01-09 18:34:43 +0800

    feat: 基本选择UI构建

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'common/theme/colors.dart';
+
 class App extends StatefulWidget {
 
   const App({super.key});
@@ -21,16 +23,72 @@ class AppState extends State<App>{
           fit: StackFit.loose,
           alignment: Alignment.center,
           // overflow: Overflow.clip, // 1.22.0 被去除
-          children: <Widget>[Row(
+          children: <Widget>[Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(color: Colors.deepOrange,),
-              Container(color: Colors.blueAccent,),
+              Expanded(child: Container(color: Colors.deepOrange)),
+              Expanded(child: Container(color: Colors.blueAccent)),
             ],
+          ),Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                getButtonUI("选择图片",(){
+
+                }),
+                const SizedBox(
+                  width: 16,
+                ),
+                getButtonUI("待定",(){
+
+                }),
+              ],
+            ),
           )],
         ),
       ),
     ));
   }
 
+
+
+  /// ------------------------------------------------------------------------
+  /// Get Button UI
+  /// ------------------------------------------------------------------------
+  Widget getButtonUI(String buttonText,void Function()? tapEvent) {
+    String txt = buttonText;
+    return Wrap(
+      children: [Container(
+        decoration: BoxDecoration(
+            color: nearlyWhite,
+            borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+            border: Border.all(color: nearlyBlue)),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            splashColor: Colors.white24,
+            borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+            onTap: tapEvent,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 12, bottom: 12, left: 18, right: 18),
+              child: Center(
+                child: Text(
+                  txt,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12,
+                    letterSpacing: 0.27,
+                    color: nearlyBlue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      )],
+    );
+  }
 }
---
+++ lib/common/theme/colors.dart
@@ -0,0 +1,5 @@
+
+import 'package:flutter/material.dart';
+
+const Color nearlyWhite = Color(0xFFFFFFFF);
+const Color nearlyBlue = Color(0xFF00B6F0);
